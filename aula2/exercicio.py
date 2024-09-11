727226f9efc80563fac61de4f34c45b5216fffe8
Vanessacelestrini <vanessaxpc@gmail.com>
2024-09-11 11:48:58 -0300

    Add files via upload

---
+++ aula2/exercicio.py
@@ -0,0 +1,14 @@
+""" 
+Escreva um programa de leia um valor em metros e exiba covertido em milimetros
+
+"""
+
+# Entrada
+metros = input("Digite o valor em metros")
+
+# Processamento
+
+milimetro = 1000 * metros
+
+# Saida
+print(f'{metros} em milimetro são {milimetro}')
---
+++ aula2/set.py
@@ -0,0 +1,11 @@
+alunos = ["simone","aurélio","fernando","gustuvo"]
+            #0       1           2          3
+print(alunos)
+#print(alunos[]) #indexError: list index out of range
+ 
+alunos.extend("marcela")
+
+print(alunos)
+alunos.extend(["eduarda"])
+print(alunos)
+
