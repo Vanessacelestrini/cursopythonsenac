b2a7f860314f57a40ac03b60d48ad6b3e449f5d0
Vanessacelestrini <vanessaxpc@gmail.com>
2025-01-13 11:38:53 -0300

    Add files via upload

---
+++ techNova/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('contato/', views.contato, name='contato'),
+    path('servicos/', views.servicos, name='servicos'),
+    path('cursos/', views.cursos, name='cursos'),
+    path('registrar/', views.registrarUsuario, name='registrar-usuario'),
+    path('login/', views.login, name='login'),
+]
